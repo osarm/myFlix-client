607c96b07b8da5bce21215b4f9892ccef60b97ec
osarm <161492930+osarm@users.noreply.github.com>
2024-09-19 23:36:39 -0400

    Rendering my Data in my Components
    If applied, this commit fetch my data from my myFlix API. Adds proptypes to update my components

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,26 +1,37 @@
+import React from "react";
+import PropTypes from "prop-types";
+
 export const MovieView = ({ movie, onBackClick }) => {
-    return (
-      <div>
-        <div>
-          <img src={movie.image} alt={movie.title} />
-        </div>
-        <div>
-          <span>Title: </span>
-          <span>{movie.title}</span>
-        </div>
-        <div>
-          <span>Description: </span>
-          <span>{movie.description}</span>
-        </div>
-        <div>
-          <span>Genre: </span>
-          <span>{movie.genre}</span>
-        </div>
-        <div>
-          <span>Director: </span>
-          <span>{movie.director}</span>
-        </div>
-        <button onClick={onBackClick}>Back</button>
-      </div>
-    );
+  return (
+    <div>
+      <h1>{movie.Title}</h1> {/* Show title */}
+      <p><strong>Director:</strong> {movie.Director.Name}</p> {/* Show director */}
+      <p><strong>Description:</strong> {movie.Description}</p> {/* Show description */}
+      <p><strong>Genre:</strong> {movie.Genre.Name}</p> {/* Show genre */}
+      <button onClick={onBackClick}>Back</button> {/* Back button */}
+    </div>
+  );
+};
+
+  MovieView.propTypes = {
+    movie: PropTypes.shape({
+      _id: PropTypes.string.isRequired,
+      Title: PropTypes.string.isRequired,
+      Description: PropTypes.string.isRequired,
+      Genre: PropTypes.shape({
+        Name: PropTypes.string.isRequired
+      })
+      .isRequired,
+      Director: PropTypes.shape({
+        Name: PropTypes.string.isRequired,
+        Bio: PropTypes.string,
+        Birth: PropTypes.string,
+        Death: PropTypes.string
+      })
+      .isRequired,
+      ImagePath: PropTypes.string.isRequired,
+      Featured: PropTypes.bool.isRequired
+    })
+    .isRequired,
+    onBackClick: PropTypes.func.isRequired
   };

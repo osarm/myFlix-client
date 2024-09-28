8d866dd99f6d2941e5a8a0a8fb20f098919822ba
osarm <161492930+osarm@users.noreply.github.com>
2024-09-28 15:39:56 -0400

    Updating mainview.jsx
    If applied, this commit will allow the login and signup view to be integrated with mainview. Should send the authentication token along with fetch request to get all movies.

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,19 +1,34 @@
 import { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
+import { LoginView } from "../login-view/login-view";
+import { SignupView } from "../signup-view/signup-view";
 
 export const MainView = () => {
+  const storedUser = JSON.parse(localStorage.getItem("user"));
+  const storedToken = localStorage.getItem("token");
   const [movies, setMovies] = useState([]);
   const [selectedMovie, setSelectedMovie] = useState(null);
+  const [error, setError] = useState(null);
+  const [user, setUser] = useState(storedUser);
+  const [token, setToken] = useState(storedToken);
 
   useEffect(() => {
-    fetch("https://movies-fx-6586d0468f8f.herokuapp.com/movies")
-      .then((response) => response.json())
-      .then((data) => {
+    if (!token) return;
+
+    fetch("https://movies-fx-6586d0468f8f.herokuapp.com/movies", {
+      headers: { Authorization: `Bearer ${token}`},
+    })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then((data) => {
         console.log(data); // Log fetched data
         
-        const moviesFromApi = data.map((movie) => {
-          return {
+        const moviesFromApi = data.map((movie) => ({
             _id: movie._id,
             Title: movie.Title,
             Description: movie.Description,
@@ -21,13 +36,32 @@ export const MainView = () => {
             Director: movie.Director,
             ImagePath: movie.ImagePath,
             Featured: movie.Featured,
-          };
-        });
-  
+          }));
         setMovies(moviesFromApi);
       })
-      .catch((error) => console.error('Error fetching movies:', error));
-  }, []);
+      .catch((error) => {
+        console.error("Error fetching movies:", error);
+        setError(error.message);
+      });
+  }, [token]);
+
+  if (error) {
+    return <div>Error: {error}</div>
+  }
+
+  if (!user) {
+    return (
+      <>
+        <LoginView 
+        onLoggedIn={(user, token) => {
+          setUser(user);
+          setToken(token);
+        }} />
+        or
+        <SignupView />
+      </>
+    );
+  }
 
     if (selectedMovie) {
       return (
@@ -50,6 +84,7 @@ export const MainView = () => {
             onMovieClick={(newSelectedMovie) => setSelectedMovie(newSelectedMovie)}
           />
         ))}
+        <button onClick={() => { setUser(null); setToken(null); localStorage.clear(); }}>Logout</button>
       </div>
     );
   };
